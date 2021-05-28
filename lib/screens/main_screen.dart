c7957f39c1d14fc52edc73cde37364645ec0de6f
oongus <oognus@gmail.com>
2021-05-28 11:58:51 +0900

    미션: 경기장 리스트를 만들고 누르면 디테일 화면에 경기장 이름과 사진 보여주기

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
 
+//
+import 'package:plab_first_project/models/Stadium.dart';
+
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text('메인 페이지입니다'),
-          onPressed: () {
-            // Named route를 사용하여 두 번째 화면으로 전환합니다.
-            Navigator.pushNamed(context, '/detail');
-          },
-        ),
-      ),
+      // body: Center(
+      //   child: ElevatedButton(
+      //     child: Text('메인 페이지입니다'),
+      //     onPressed: () {
+      //       // Named route를 사용하여 두 번째 화면으로 전환합니다.
+      //       Navigator.pushNamed(context, '/detail');
+      //     },
+      //   ),
+      // ),
+      body: MainListExample(),
+    );
+  }
+}
+
+
+class MainListExample extends StatefulWidget {
+  
+  @override
+  _MainListState createState() => _MainListState();
+}
+
+class _MainListState extends State<MainListExample> {
+
+  @override
+  initState() {
+    //초기화
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Stadium>> (
+      future: Stadium.fetchStadiums(),
+      builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        Stadium stadium = snapshot.data[0];
+        return Center(
+          child: Text(stadium.name),
+          //응답 받은 데이터로 UI 그리기
+        );
+      }
+
+      return CircularProgressIndicator();
+      }
     );
   }
 }
